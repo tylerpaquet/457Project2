779facda2f5907b0ee6199be302e4efcfc1261c4
markerj <markerj@mail.gvsu.edu>
2017-10-02 12:21:16 -0400

    file transfer for one byte of a file

---
+++ Client3.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+	private DatagramSocket socket;
+	private int portNum;
+	private InetAddress ipAddr;
+	private File file;
+	private FileOutputStream fos;
+	public Client(int port, String ip, String fileName){
+		try {
+			socket = new DatagramSocket();
+
+			ipAddr = InetAddress.getByName(ip);
+			portNum = port;
+
+			byte[] outBytes = fileName.getBytes();
+			DatagramPacket outPacket = 
+					new DatagramPacket( outBytes, outBytes.length,ipAddr,portNum);
+			System.out.println("requesting file from server...");
+			socket.send(outPacket);	
+
+			file = new File("downloaded" + fileName);
+			fos = new FileOutputStream(file);
+			try{
+				byte[] incBytes = new byte[1024];
+				DatagramPacket incPacket = new DatagramPacket( incBytes, incBytes.length );
+				socket.receive(incPacket);
+				fos.write(incBytes);
+			}
+			catch(IOException e){}
+		}catch(IOException e){
+		}
+	}
+
+	public static void main(String[] args){
+		Client client = new Client(Integer.valueOf(args[0]), args[1],args[2]);
+	}
+}
