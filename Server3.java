1f3d18d5645674cfd0a017501c853fc056b71c8e
markerj <markerj@mail.gvsu.edu>
2017-10-02 12:19:54 -0400

    file transfer for one byte of a file

---
+++ Server3.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server3 {
+	
+	private DatagramSocket socket;
+	private int portNum;
+	private InetAddress ipAddr;
+	private FileInputStream fis;
+	
+	public Server(int port){
+		try {
+			System.out.println("Waiting for client connection");
+			socket = new DatagramSocket(port);
+			
+			while(true){
+				System.out.println("receiving client message");
+				byte[] incBytes = new byte[ 1024 ];
+				DatagramPacket incPacket = new DatagramPacket(incBytes,incBytes.length );
+				socket.receive(incPacket);
+				
+				ipAddr = incPacket.getAddress();
+				portNum = incPacket.getPort();
+				
+				String fileName = new String(incPacket.getData(),0,incPacket.getLength());
+				System.out.println("Client requesting file: "+ fileName);
+				File file = new File(fileName);
+				fis = new FileInputStream(file);
+				
+				//initalize
+				if(fis.available() >0){
+					byte bytes[] = new byte[1024];
+					fis.read(bytes);
+					DatagramPacket Packet = new DatagramPacket(bytes,bytes.length,ipAddr,portNum);
+					socket.send(Packet);
+					System.out.println("Sending packet");
+					
+				}
+			}
+			
+			
+		}catch(IOException e){}
+		
+		
+	}
+	public static void main(String[] args){
+		Server server = new Server(Integer.valueOf(args[0]));
+	}
+}
