459c97fa0a75ca593c2146d7d6952f303997f472
Tyler Paquet <paquett@mail.gvsu.edu>
2017-10-03 11:42:41 -0400

    Simplified logic. Still need to implement sliding window

--- Server.java
+++ Server.java
@@ -54,53 +54,37 @@ class Server
 				
 				//Send file to client in packet sizes of 1024 bytes Sliding window of 5 packets
 				
-				//File fits in one packet
-				if(fileSize <= 1024)
-				{
-					byte[] sendData = new byte[(int)fileSize];
-					fis.read(sendData);
-					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
-					serverSocket.send(sendPacket);
-				}
-				
-				//File must be sent using multiple packets
-				else
-				{
-					int numPackets = ((int)fileSize/1024) + 1;
-					int numBytesLastPacket = (int)fileSize%1024;
-					//System.out.println("Number of packets = " + numPackets);
-					//System.out.println("Number of bytes in final packet = " + numBytesLastPacket);
+				int numPackets = ((int)fileSize/1024) + 1;
+				int numBytesLastPacket = (int)fileSize%1024;
+				//System.out.println("Number of packets = " + numPackets);
+				//System.out.println("Number of bytes in final packet = " + numBytesLastPacket);
 					
-					//byte[] fileData = new byte[(int)fileSize];
-					//fis.read(fileData);
-					//int offset = 0;
+				//byte[] fileData = new byte[(int)fileSize];
+				//fis.read(fileData);
+				//int offset = 0;
 					
-					for(int i = 0; i < numPackets; i++)
+				for(int i = 0; i < numPackets; i++)
+				{
+					System.out.println("Sending Packet #" + (i+1) + "/" + numPackets);
+					if(i == numPackets - 1)
 					{
-						System.out.println("Sending Packet #" + (i+1) + "/" + numPackets);
-						if(i == numPackets - 1)
-						{
-							byte[] sendData = new byte[numBytesLastPacket];
-							fis.read(sendData);
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
-							serverSocket.send(sendPacket);
-						}
-						else
-						{
-							byte[] sendData = new byte[1024];
-							fis.read(sendData);
-							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
-							serverSocket.send(sendPacket);
-						}
-						
-						byte[] ack = new byte[1024];
-						DatagramPacket receiveAck = new DatagramPacket(ack, ack.length);
-						serverSocket.receive(receivePacket);
-						
-						
-						
+						byte[] sendData = new byte[numBytesLastPacket];
+						fis.read(sendData);
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
+						serverSocket.send(sendPacket);
 					}
-					
+					else
+					{
+						byte[] sendData = new byte[1024];
+						fis.read(sendData);
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
+						serverSocket.send(sendPacket);
+					}
+						
+					byte[] ack = new byte[1024];
+					DatagramPacket receiveAck = new DatagramPacket(ack, ack.length);
+					serverSocket.receive(receivePacket);		
+						
 				}
 				
 			}
