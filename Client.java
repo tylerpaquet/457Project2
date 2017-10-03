7300c8bda89946fb642a854e0f3cc3a255bf9ab9
Tyler Paquet <paquett@mail.gvsu.edu>
2017-10-03 11:03:39 -0400

    File transfer now works with sliding window of 1 (stop and wait)

--- Client.java
+++ Client.java
@@ -41,13 +41,82 @@ class Client
 				else if(userInput.substring(0, 4).toLowerCase().equals("send"))
 				{
 					//Send file request to server
-					byte[] sendData = new byte[1024];
+					byte[] sendData = new byte[userInput.length()];
 					sendData = userInput.getBytes();
 					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portNum);
 					clientSocket.send(sendPacket);
 					
+					
+					//Receive File Size from Server
+					byte[] receiveFileSize = new byte[1024];
+					DatagramPacket receiveFileSizePacket = new DatagramPacket(receiveFileSize, receiveFileSize.length);
+					clientSocket.receive(receiveFileSizePacket);
+					String fileSize = new String(receiveFileSizePacket.getData());
+					int length = receiveFileSizePacket.getLength();
+					String fileSize2 = fileSize.substring(0, length);
+					//System.out.println("File size string: " + fileSize);
+					int fileSizeInt = Integer.parseInt(fileSize2);
+					//System.out.println("File size int: " + fileSizeInt); //prints file size in bytes
+					
+					
 					//receive file packets (1024 bytes each) and save to a new file
-					//Sliding window of 5 packets
+					
+					//File can be sent in one packet
+					if(fileSizeInt <= 1024)
+					{
+						File file = new File("downloaded" + userInput.substring(5, userInput.length()));
+						FileOutputStream fos = new FileOutputStream(file);
+					
+						byte[] receiveData = new byte[1024];
+						DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+						clientSocket.receive(receivePacket);
+					
+						int packetSize = receivePacket.getLength();
+						fos.write(receiveData, 0, packetSize);
+					}
+					
+					//File must be sent using multiple packets
+					else
+					{
+						int numPackets = ((int)fileSizeInt/1024) + 1;
+						int numBytesLastPacket = (int)fileSizeInt%1024;
+						//System.out.println("Number of packets = " + numPackets);
+						//System.out.println("Number of bytes in final packet = " + numBytesLastPacket);
+						
+						File file = new File("downloaded" + userInput.substring(5, userInput.length()));
+						FileOutputStream fos = new FileOutputStream(file);
+						
+						byte[] fileData = new byte[fileSizeInt];
+						int offset = 0;
+						
+						for(int i = 0; i < numPackets; i++)
+						{
+							System.out.println("Receiving Packet #" + (i+1) + "/" + numPackets);
+							if(i == numPackets - 1)
+							{
+								byte[] receiveData = new byte[numBytesLastPacket];
+								DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								clientSocket.receive(receivePacket);
+								fos.write(receiveData);
+								
+							}
+							else
+							{
+								byte[] receiveData = new byte[1024];
+								DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+								clientSocket.receive(receivePacket);
+								fos.write(receiveData);
+							}
+							
+							String ackStr = "received packet " + (i+1); 
+							byte[] ack = new byte[ackStr.length()];
+							ack = ackStr.getBytes();
+							DatagramPacket sendAck = new DatagramPacket(ack, ack.length, ipAddress, portNum);
+							clientSocket.send(sendAck);
+							
+						}
+					}
+					
 				}
 				else
 				{
--- Server.java
+++ Server.java
@@ -10,6 +10,7 @@ import java.io.*;
 import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
+import static java.lang.Math.toIntExact;
 
 class Server
 {
@@ -31,17 +32,83 @@ class Server
 				String sentence = new String(receivePacket.getData());
 				System.out.println("From client: " + sentence);
 				
-				String fileToSend = sentence.substring(5, sentence.length());
+				int length = receivePacket.getLength();
+				String fileToSend = sentence.substring(5, length);
 				File myFile = new File(fileToSend);
+				FileInputStream fis = new FileInputStream(myFile);
+				InetAddress ipAddress = receivePacket.getAddress();
+				int portnumber = receivePacket.getPort();
 				
-				//Send file to client in packet sizes of 1024 bytes
-				//Sliding window of 5 packets
-				byte[] sendData = new byte[1024];
+				
+				long fileSize = myFile.length();
+				//System.out.println("File size: " + fileSize);
+				
+	
+				//Send file size to client
+				String fileSizeStr = Long.toString(fileSize);
+				byte[] sendFileSize = new byte[fileSizeStr.length()];
+				sendFileSize = fileSizeStr.getBytes();
+				DatagramPacket sendFileSizePacket = new DatagramPacket(sendFileSize, sendFileSize.length, ipAddress, portnumber);
+				serverSocket.send(sendFileSizePacket);
+				
+				
+				//Send file to client in packet sizes of 1024 bytes Sliding window of 5 packets
+				
+				//File fits in one packet
+				if(fileSize <= 1024)
+				{
+					byte[] sendData = new byte[(int)fileSize];
+					fis.read(sendData);
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
+					serverSocket.send(sendPacket);
+				}
+				
+				//File must be sent using multiple packets
+				else
+				{
+					int numPackets = ((int)fileSize/1024) + 1;
+					int numBytesLastPacket = (int)fileSize%1024;
+					//System.out.println("Number of packets = " + numPackets);
+					//System.out.println("Number of bytes in final packet = " + numBytesLastPacket);
+					
+					//byte[] fileData = new byte[(int)fileSize];
+					//fis.read(fileData);
+					//int offset = 0;
+					
+					for(int i = 0; i < numPackets; i++)
+					{
+						System.out.println("Sending Packet #" + (i+1) + "/" + numPackets);
+						if(i == numPackets - 1)
+						{
+							byte[] sendData = new byte[numBytesLastPacket];
+							fis.read(sendData);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
+							serverSocket.send(sendPacket);
+						}
+						else
+						{
+							byte[] sendData = new byte[1024];
+							fis.read(sendData);
+							DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, portnumber);
+							serverSocket.send(sendPacket);
+						}
+						
+						byte[] ack = new byte[1024];
+						DatagramPacket receiveAck = new DatagramPacket(ack, ack.length);
+						serverSocket.receive(receivePacket);
+						
+						
+						
+					}
+					
+				}
+				
 			}
 		}
 		catch(IOException e)
 		{
-			System.out.println("Got exception");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
